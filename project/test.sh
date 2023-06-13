dd61d0ba4a81e904081b38b902e318fbc00913af
chitraahuja <CookChoco@46>
2023-06-13 18:49:03 +0200

    wip

--- project/test.sh
+++ project/test.sh
@@ -2,9 +2,9 @@
 
 # execute the pipeline
 echo "Execute the pipeline"
-python ../data/script.py
+python ./data/script.py
 
 # test if pipeline works correct
 echo "Test if pipeline works correctly"
-pytest ../data/script_test.py
+pytest ./data/script_test.py
 
